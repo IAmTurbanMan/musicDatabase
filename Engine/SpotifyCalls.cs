5960a7d2bb8e2f3d67de7fd08bba18e81eec1cc7
Rash Kader <rashidul.kader@gmail.com>
2020-09-26 12:06:18 -0700

    Added comments and made the connection request it's own method

--- Engine/SpotifyCalls.cs
+++ Engine/SpotifyCalls.cs
@@ -10,17 +10,28 @@ namespace Engine
 {
 	public static class SpotifyCalls
 	{
+		//set variables for API keys
+		//gets values from app.config file to keep them hidden
 		private static string CLIENT_ID = ConfigurationManager.AppSettings["CLIENT_ID"];
 		private static string CLIENT_SECRET = ConfigurationManager.AppSettings["CLIENT_SECRET"];
+		static SpotifyClient Spotify;
 
-		public static async Task<string> GetArtist()
+		//private method to request a connection to the API
+		private static async Task RequestConnection()
 		{
 			var Config = SpotifyClientConfig.CreateDefault();
 
 			var Request = new ClientCredentialsRequest(CLIENT_ID, CLIENT_SECRET);
 			var Response = await new OAuthClient(Config).RequestToken(Request);
 
-			var Spotify = new SpotifyClient(Config.WithToken(Response.AccessToken));
+			Spotify = new SpotifyClient(Config.WithToken(Response.AccessToken));
+		}
+
+		//method to get Artist name based on unique Artist ID
+		//currently using a placeholder ID for Pitbull
+		public static async Task<string> GetArtist()
+		{
+			await RequestConnection();
 
 			var Artist = await Spotify.Artists.Get("0TnOYISbd1XYRBk9myaseg");
 			string ArtistName = Artist.Name;
